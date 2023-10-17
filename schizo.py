99c030e35b4a7d1d987ff83ae3d9bcd8b79b0e08
biorpg <biorpg2@hotmail.com>
2023-10-17 03:48:22 -0700

    shelved unused portions of the code

--- schizo.py
+++ schizo.py
@@ -1,6 +1,5 @@
 import os
 import openai
-import tiktoken
 import copy
 
 openai.api_key = "sk-111111111111111111111111111111111111111111111111"
@@ -15,97 +14,10 @@ requesttext = ""
 tokenlength = tokenlength + len(contextuser1.split(" "))
 tokenlength = tokenlength + len(contextresponse.split(" "))
 messages = [
-    {'role': 'system',
-     'content': "Answer in a manner consistent with the request. Please make use of the memorize() function to store any information you wish to be remembered. The following formats are acceptable: memorize(trigger: 'subject', content: 'content', mtype: 'conversation', purpose: 'context', sentiment: 'neutral', weight: 1.0, contextualdepth: 3) or memorize {trigger = 'subject', content = 'content', mtype = 'conversation', purpose = 'context', sentiment = 'neutral', weight = 1.0, contextualdepth = 3}. 'trigger', one or two_word symbol indicating when this memory should be recalled. 'content' is the content of the memory, it may be left blank if the content is to be the message history. 'mtype' is the type of memory. 'purpose' is the purpose of the memory. 'sentiment' is the sentiment of the memory. 'weight' is the weight of the memory. 'contextualdepth' is the number of previous messages to include in the memory."},
+    {'role': 'system', 'content': "Answer in a manner consistent with the request."},
     {'role': 'user', 'content': contextuser1},
     {'role': 'assistant', 'content': contextresponse}
 ]
-print(tokenlength)
-
-
-def outputdirector(role, output):
-    # use regex to extract anything wrapped in brackets, and delimited by commas
-    if "memorize(" in output:
-        outputobject = output.split("memorize")[1].split(")")[0]
-        outputobject = outputobject.replace("(", "")
-        outputobject = outputobject.split(",")
-        for o in outputobject:
-            if "trigger" in o:
-                trigger = o.split(":")[1].replace('"', '')
-            elif "content" in o:
-                content = o.split(":")[1].replace('"', '')
-            elif "mtype" in o:
-                mtype = o.split(":")[1].replace('"', '')
-            elif "purpose" in o:
-                purpose = o.split(":")[1].replace('"', '')
-            elif "sentiment" in o:
-                sentiment = o.split(":")[1].replace('"', '')
-            elif "weight" in o:
-                weight = o.split(":")[1].replace('"', '')
-            elif "contextualdepth" in o:
-                contextualdepth = o.split(":")[1].replace('"', '')
-    elif "memorize" in output:
-        outputobject = output.split("{")[1].split("}")[0]
-        outputobject = outputobject.split(",")
-        for o in outputobject:
-            if "trigger" in o:
-                trigger = o.split("=")[1].replace('"', '')
-            elif "content" in o:
-                content = o.split("=")[1].replace('"', '')
-            elif "mtype" in o:
-                mtype = o.split("=")[1].replace('"', '')
-            elif "purpose" in o:
-                purpose = o.split("=")[1].replace('"', '')
-            elif "sentiment" in o:
-                sentiment = o.split("=")[1].replace('"', '')
-            elif "weight" in o:
-                weight = o.split("=")[1].replace('"', '')
-            elif "contextualdepth" in o:
-                contextualdepth = o.split("=")[1].replace('"', '')
-    else:
-        return
-    if not trigger:
-        trigger = "subject"
-    if not content:
-        content = ""
-    if not mtype:
-        mtype = "conversation"
-    if not purpose:
-        purpose = "context"
-    if not sentiment:
-        sentiment = "neutral"
-    if not weight:
-        weight = 1.0
-    if not contextualdepth:
-        contextualdepth = 3
-    memorize(role, trigger, content, mtype, purpose, sentiment, weight, contextualdepth)
-
-
-def memorize(role='system', trigger="subject", content="", mtype="conversation", purpose="context", sentiment="neutral",
-             weight=1.0, contextualdepth=3):
-    global messages
-    if type == "conversation":
-        if content == "":
-            for m in range(0, contextualdepth):
-                content = content + '\n' + messages[m]['content']
-    openai.api_base = "http://192.168.2.2:5001/v1"
-    # create a directory for storing various memories and information
-    memory = [
-        {'role': role, 'content': content, 'type': mtype, 'purpose': purpose, 'sentiment': sentiment, 'weight': weight,
-         'trigger': trigger}
-    ]
-    if not os.path.exists("memories"):
-        os.mkdir("memories")
-    if not os.path.exists("memories/" + role):
-        os.mkdir("memories/" + role)
-    # save the conversation so far
-    with open("memories/" + role + "/" + mtype + ".txt", "a") as f:
-        f.write(str())
-        f.write("\n")
-        f.close()
-
-    print(f"\033[92m{role} stored a .\033[0m")
-
 
 def crunchbot():
     global tokenlength, messages
@@ -116,16 +28,14 @@ def crunchbot():
     messages_copy.pop(0)
     first_message = messages_copy[0]
     second_message = messages_copy[1]
-    print(tokenlength)
     tokenlength = 8
     tokenlength = tokenlength + len(last_message['content'].split(" "))
     tokenlength = tokenlength + len(second_last_message['content'].split(" "))
     tokenlength = tokenlength + len(first_message['content'].split(" "))
     tokenlength = tokenlength + len(second_message['content'].split(" "))
 
-    print(tokenlength)
     crunchmessage = [
-        {'role': 'system', 'content': "Summarize the following discussion as consisely as possible."}
+        {'role': 'system', 'content': "Paraphrase the original request, and briefly cover the discussion's progress as consisely as possible. If the last several messages begin with congratulatory remarks, end with promisary remarks, or contain closing remarks from both parties, omit those remarks from your summary, and more aggressively reintroduce the original topic in your paraphrasal."}
     ]
     for m in messages_copy:
         crunchmessage.append(m)
@@ -166,38 +76,18 @@ def refereebot():
 
     for m in messages_copy:
         refereemessage.append(m)
+
     refereeeresponse = openai.ChatCompletion.create(
         model="x",
         messages=refereemessage
     )
-    # print the original message in grey font
+
     refereetext = refereeeresponse['choices'][0]['message']['content']
     if (refereetext != "continue") or (refereetext != "Continue"):
         print(f"\033[90mSystem: {refereetext}\033[0m")
         tokenlength = tokenlength + len(refereetext.split(" "))
-        print(tokenlength)
         messages.append({'role': 'system', 'content': refereetext})
     return
-
-
-def scrutinize(responsetext):
-    global tokenlength, messages
-    openai.api_base = "http://192.168.2.2:5001/v1"
-    last_message = messages[-1]
-    scrutinizemessage = [
-        {'role': 'user', 'content': last_message['content']},
-        {'role': 'system',
-         'content': "Edit the following message to be coherent, realistic, yet maligned and protagonistic in the context of the previous message. If the message indicates that the participants are awaiting an event, then append a statement indicating that the event has occurred."},
-        {'role': 'user', 'content': responsetext}
-    ]
-    scrutinizeresponse = openai.ChatCompletion.create(
-        model="x",
-        messages=scrutinizemessage
-    )
-    # print the original message in grey font
-
-    print(f"\033[90mScrutinizing: {responsetext}\033[0m")
-    return scrutinizeresponse['choices'][0]['message']['content']
 
 
 def assistantbot():
@@ -209,22 +99,17 @@ def assistantbot():
             message['role'] = 'assistant'
         elif message['role'] == 'assistant':
             message['role'] = 'user'
-    # Make API call
     response = openai.ChatCompletion.create(
         model="x",
         messages=messages
     )
     # Extract and print the assistant's reply
     responsetext = response['choices'][0]['message']['content']
-    # responsetext = scrutinize(responsetext)
     print(f"\033[92mBot1: {responsetext}\033[0m")
 
     # Append the assistant's reply to 'messages'
-    # tokenizedresponse = tiktoken.Encoding.encode(responsetext)
     tokenlength = tokenlength + len(responsetext.split(" "))
-    print(tokenlength)
     messages.append({'role': 'assistant', 'content': responsetext})
-    outputdirector('Bot1', responsetext)
 
 
 def userbot():
@@ -236,31 +121,19 @@ def userbot():
             message['role'] = 'assistant'
         elif message['role'] == 'assistant':
             message['role'] = 'user'
-    # Make API call
     response = openai.ChatCompletion.create(
         model="x",
         messages=messages
     )
     # Extract and print the assistant's reply
     requesttext = response['choices'][0]['message']['content']
-    # requesttext = scrutinize(requesttext)
     print(f"\033[92mBot2: {requesttext}\033[0m")
 
     # Append the assistant's reply to 'messages'
-    # tokenizedrequest = tiktoken.Encoding.encode(requesttext)
     tokenlength = tokenlength + len(requesttext.split(" "))
-    print(tokenlength)
     messages.append({'role': 'assistant', 'content': requesttext})
-    outputdirector('Bot2', requesttext)
 
 
-# def commandbot():
-#     openai.api_base = "http://192.168.2.1:5001/v1"
-#     messages = [
-#         {'role': 'system', 'content': ""},
-#         {'role': 'user', 'content': contextuser1},
-#         {'role': 'assistant', 'content': contextresponse}
-#     ]
 while True:
     try:
         assistantbot()
